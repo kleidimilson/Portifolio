20dc7f98e4974b28ee3838eef166ca2330ec9ed3
kleidimilson <kleidimilson@gmail.com>
2020-04-06 17:49:05 -0300

    home

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <header>
             <nav id="menu" class="navbar navbar-expand-md fixed-top navbar-light">
                 <div class="container">
-                    <a href="index.html" class="navbar-brand" id="h2-logo">WEB<spam class="spam">   DEV  </spam></a>
+                    <a href="https://kleidimilson.herokuapp.com/" class="navbar-brand" id="h2-logo">WEB<spam class="spam">   DEV  </spam></a>
                     <button class="navbar-toggler text-white" data-toggle="collapse" data-target="#nav">
                         <i class="fas fa-bars " id="toggle"></i>
                     </button>
